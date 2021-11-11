c072d34dde288df8ad060fce4339bd8c4302966d
sodizdaroglu <americanlaz@gmail.com>
2021-11-11 10:03:59 -0500

    footer

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,10 +1,12 @@
 import Profile from './PortfolioContainer/Home/Profile';
+import Footer from './PortfolioContainer/Home/Footer/Footer';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
      <Profile />
+     <Footer/>
     </div>
   );
 }
