97fe8c31f8b23a82af409bf7757b6f642cb70c92
sodizdaroglu <americanlaz@gmail.com>
2021-11-16 14:38:59 -0500

    styling

--- portfolio-site/src/PortfolioContainer/Home/Header/Header.js
+++ portfolio-site/src/PortfolioContainer/Home/Header/Header.js
@@ -10,33 +10,39 @@ export default function Header() {
   const [showHeaderOptions, setShowHeaderOptions] = useState(false);
 
   const updateCurrentScreen = (currentScreen) => {
-    if (!currentScreen || !currentScreen.screenInView) 
-    return;
-    let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
-    if (screenIndex < 0) 
-    return;
-  }
+    if (!currentScreen || !currentScreen.screenInView)  return;
 
-  let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
+    let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
+    if (screenIndex < 0)  return;
+  };
+
+
+
+ let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
   const getHeaderOptions = () => {
-    return (
-        TOTAL_SCREENS.map((screen, i) => (
-           <div key={screen.screen_name} className={getHeaderOptionsClass(i)}
-               onClick={() => switchScreen(i, screen)} >
-                <span>{screen.screen_name}</span>
+    return TOTAL_SCREENS.map((Screen, i) => (
+           <div key={Screen.screen_name} className={getHeaderOptionsClasses(i)}
+               onClick={() => switchScreen(i, Screen)} >
+                <span>{Screen.screen_name}</span>
            </div>
       ))
-    )
+    
   };
 
-  const getHeaderOptionsClass = (index) => {
-    let classes = "header-option";
-    if (index < TOTAL_SCREENS.length - 1) classes += "header-options-seperator";
 
-    if (selectedScreen === index) classes += "selected-header-option";
-    return ;
-  };
+
+   const getHeaderOptionsClasses = (index) => {
+   let classes = "header-option ";
+  
+   if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator ";
+
+   if (selectedScreen === index) classes += "selected-header-option ";
+ 
+    return classes;
+  
+   };
+
 
   const switchScreen = (index, screen) => {
     let screenComponent = document.getElementById(screen.screen_name);
@@ -48,20 +54,37 @@ export default function Header() {
   };
 
   return (
-    <div>
-      <div className="header-container" onClick={() => setShowHeaderOptions(!showHeaderOptions)}>
-        <div className="header-parent">
-            <div className="header-hamburger" onClick={() => setShowHeaderOptions(!showHeaderOptions)}>
-               <FontAwesomeIcon className='header-hamburger-bars' icon={faBars} />
-            </div>
-            <div className='header-logo'>
-                <span>OGUZ</span>
-            </div>
-            <div className={(showHeaderOptions) ? "header-options show-hamburger-options" : "header-options"}>
-                {getHeaderOptions()}
-            </div>
+    <div
+      className="header-container"
+      onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+    >
+      <div className="header-parent">
+        <div
+          className="header-hamburger"
+          onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+        >
+          <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+        </div>
+        <div className="header-logo">
+          <span>OGUZ</span>
+        </div>
+        <div
+          className={
+            showHeaderOptions
+              ? "header-options show-hamburger-options"
+              : "header-options"
+          }
+        >
+          {getHeaderOptions()}
         </div>
       </div>
     </div>
   )
 }
+
+
+
+
+
+  
+
